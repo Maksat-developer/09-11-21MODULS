e4dff05ec96eb3d9da089b43700ebbbab96a4f82
Stalker <gangsta.okulus02@gmail.com>
2021-12-22 12:33:08 +0600

    BOOT

---
+++ CPP/projectCPP/projectCPP/test.cpp
@@ -0,0 +1,8 @@
+//
+//  test.cpp
+//  projectCPP
+//
+//  Created by UMBRELLA on 18.12.2021.
+//
+
+#include "test.hpp"
---
+++ CPP/projectCPP/projectCPP/test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  projectCPP
+//
+//  Created by UMBRELLA on 18.12.2021.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
